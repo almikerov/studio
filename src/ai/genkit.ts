e6550baf6e0651ed2eb2d6cfb68b6bc34d05c45e
Alexandr Mikerov <al.mikerov@gmail.com>
2025-09-26 19:35:57 +0000

    я вижу только одно изменение. Ты нияего не поменял почти

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GENKIT_API_KEY})],
+  plugins: [googleAI()],
   model: 'gemini-1.5-flash',
 });
